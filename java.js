5ff3e8e95afad077a996e7ef9fe281bdf65884e5
Dennis Brown <176719676+Chronous07@users.noreply.github.com>
2024-08-25 20:11:58 -0400

    Create EtchaSketch
    Make a grid, default size of 16x16 which will change color as mouse
    passes through each individual portion of the grid.  User can
    re-define size of the grid, up to a max of 100.  If user enters
    text instead of number, grid is reset to 16x16.

---
+++ java.js
@@ -0,0 +1,46 @@
+const gridContainer = document.querySelector(".container");
+const sizingButton = document.getElementById("sizingButton");
+
+sizingButton.addEventListener("click", resetBoard);
+createGrid(16);
+
+function resetBoard() {
+    let gridSize = Number(prompt("Please select a new grid size (max 100)"));
+    
+    if (isNaN(gridSize)){gridSize = 16;};
+
+    if(gridSize > 100) {
+        gridSize = 100;
+    };
+
+    clearBoard();
+    createGrid(gridSize);
+}
+
+function clearBoard(){
+     while(gridContainer.firstChild){
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+}
+
+function hover (e){
+    let target = e.target;
+    target.style.background = "red";
+}
+
+function createGrid(size){
+    for (i = 0; i < size; i++) {
+        let gridRow = document.createElement("div");
+
+        for (x = 0; x < size; x++) {
+            let gridSquare = document.createElement("div");
+
+            gridSquare.id = "square";
+            gridSquare.addEventListener("mouseout", hover)
+            gridRow.id = "row";
+
+            gridRow.appendChild(gridSquare);
+        }
+        gridContainer.appendChild(gridRow);
+    }
+}
